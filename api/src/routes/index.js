f779587802ae007129770ea93928a4daaa733c58
Donaldo-1997 <donaldojbm7@gmail.com>
2022-07-28 17:48:33 -0500

    ultima

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,16 +5,17 @@ const router = Router();
 
 //--------------------------IMPORT
 const shoesRouter = require("./products");
+const user = require("./users");
 const categories= require("./categories")
 const brands= require("./brands");
-// const login = require('./login')
+const login = require('./login')
 const auth_email = require('./auth_email')
 const mercadoRouter = require("./mercadopago.js")
 
 //--------------------------ROUTES
 router.use("/auth", auth_email)
-// router.use("/login", login)
-// router.use("/user/", user);
+router.use("/login", login)
+router.use("/user/", user);
 router.use("/shoes", shoesRouter);
 router.use("/categories", categories);
 router.use("/brands", brands)
