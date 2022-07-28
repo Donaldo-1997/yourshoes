286b0c5c41061f1bf0e2f19c9eb160a34aa24a18
Donaldo-1997 <donaldojbm7@gmail.com>
2022-07-28 16:32:12 -0500

    probando con google

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,11 +8,11 @@ const shoesRouter = require("./products");
 const categories= require("./categories")
 const brands= require("./brands");
 // const login = require('./login')
-// const google = require('./google')
+const google = require('./google')
 // const mercadoRouter = require("./mercadopago.js")
 
 //--------------------------ROUTES
-// router.use("/google", google)
+router.use("/google", google)
 // router.use("/login", login)
 // router.use("/user/", user);
 router.use("/shoes", shoesRouter);
