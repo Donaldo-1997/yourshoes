845e9f90ceda996d19d19aeb41d7b1bedb71f011
Donaldo-1997 <donaldojbm7@gmail.com>
2022-07-28 16:06:41 -0500

    quitando bcrypt

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,13 +8,13 @@ const shoesRouter = require("./products");
 const user = require("./users");
 const categories= require("./categories")
 const brands= require("./brands");
-const login = require('./login')
-const google = require('./google')
+// const login = require('./login')
+// const google = require('./google')
 const mercadoRouter = require("./mercadopago.js")
 
 //--------------------------ROUTES
 // router.use("/google", google)
-router.use("/login", login)
+// router.use("/login", login)
 router.use("/user/", user);
 router.use("/shoes", shoesRouter);
 router.use("/categories", categories);
