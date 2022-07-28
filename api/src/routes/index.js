a91861b8ff7521258621302e6455a76f5f947f5a
Donaldo-1997 <donaldojbm7@gmail.com>
2022-07-28 16:21:09 -0500

    dejando basico

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,25 +1,24 @@
 const { Router } = require("express");
 const router = Router();
-var logger = require('morgan');
-var session = require('express-session');
+// var logger = require('morgan');
+// var session = require('express-session');
 
 //--------------------------IMPORT
 const shoesRouter = require("./products");
-const user = require("./users");
 const categories= require("./categories")
 const brands= require("./brands");
 // const login = require('./login')
 // const google = require('./google')
-const mercadoRouter = require("./mercadopago.js")
+// const mercadoRouter = require("./mercadopago.js")
 
 //--------------------------ROUTES
 // router.use("/google", google)
 // router.use("/login", login)
-router.use("/user/", user);
+// router.use("/user/", user);
 router.use("/shoes", shoesRouter);
 router.use("/categories", categories);
 router.use("/brands", brands)
-router.use("/mercadopago", mercadoRouter)
+// router.use("/mercadopago", mercadoRouter)
 
 router.get('/', (req, res) => {
     res.send('deploy funcionando')
