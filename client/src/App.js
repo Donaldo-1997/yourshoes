4586ca0ec886c8cf54d36012f837283718862c23
Donaldo-1997 <donaldojbm7@gmail.com>
2022-08-02 12:37:59 -0500

    solucion para el deploy

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ function App() {
           Accept: "application/json",
           "Content-Type": "application/json",
           "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Origin": "*"
         },
       })
         .then((response) => {
