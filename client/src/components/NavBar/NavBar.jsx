829608655507e099aaab88d1a35fe07ad86e545d
Juan Cruz Carracedo <juann.carracedo7@gmail.com>
2022-08-04 15:36:08 -0300

    bug en navbar de renderizado condicional

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -48,13 +48,10 @@ export default function NavBar({handleReset, handleInputName, handleNameSubmit})
             <Nav.Link  className={styles.icon}>
               <Link to="/favorites"><FaHeart style={{ color: "#f87d2d" }} /></Link> 
             </Nav.Link>
-            {Object.keys(user).length ? (
-              <a href={`${window.env.URL}/auth/logout`} onClick={handleLogout} className={styles.logout_button}><BiLogOut style={{ color: "#f87d2d" }}></BiLogOut></a>
-            ) : (
               <Nav.Link  className={styles.icon}>
                  <Link to='/login'><FaUserAlt  style={{ color: "#f87d2d" }} /></Link>
               </Nav.Link>
-            )}
+            
           </Nav>
           <button className={styles.createProdButton}>
             <Link to='/post'><span className={styles.link}>Crear producto</span></Link>
