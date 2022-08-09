25885f20264e17ea751debd8705576e43e6d7ad2
benjamalo <benjamalo02@gmail.com>
2022-08-09 15:31:32 -0300

    div si no se encuentra filtros

--- client/src/components/ProductCards/ProductCards.jsx
+++ client/src/components/ProductCards/ProductCards.jsx
@@ -4,19 +4,25 @@ import styles from "./ProductCards.module.css";
 export default function ProductCards({ allProducts }) {
   return (
     <div className={styles.container}>
-      {allProducts && allProducts.map((product) => {
-        return (
-          <div key={product.id} className={styles.card}>
-            <ProductCard
-              image={product.image}
-              title={product.title}
-              price={product.price}
-              id={product.id}
-              key={product.id}
-            />
-          </div>
-        );
-      })}
+      {allProducts.length ? (
+        allProducts.map((product) => {
+          return (
+            <div key={product.id} className={styles.card}>
+              <ProductCard
+                image={product.image}
+                title={product.title}
+                price={product.price}
+                id={product.id}
+                key={product.id}
+              />
+            </div>
+          );
+        })
+      ) : (
+        <div className={styles.sinFiltro}>
+          <h1>No se encontraron zapatos con estos filtros...</h1>
+        </div>
+      )}
     </div>
   );
 }
