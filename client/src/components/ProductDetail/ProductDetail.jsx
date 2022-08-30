30b2551fc4609bcc23b26a85c4d7d5472cbf8a36
GiseTaboada1990 <giseltaboada1990@gmail.com>
2022-08-30 18:00:07 -0300

    sacamos todo de reviews

--- client/src/components/ProductDetail/ProductDetail.jsx
+++ client/src/components/ProductDetail/ProductDetail.jsx
@@ -9,32 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Navbar2 from "../Navbar2/Navbar2";
 import { useNavigate } from 'react-router-dom'
 import { useState } from "react";
-import TextField from "@material-ui/core/TextField";
-import Rating from "@material-ui/lab/Rating";
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import { Chip } from "@material-ui/core";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import Snackbar from "@material-ui/core/Snackbar";
-import MuiAlert from "@material-ui/lab/Alert";
-import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(2),
-      width: "90%",
-    },
-  },
-}));
-function Alert(props) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
 
 export default function ProductDetail({ id }) {
   const [description, setDescription] = useState("");
@@ -44,7 +19,6 @@ export default function ProductDetail({ id }) {
   const [open, setOpen] = useState(false);
   const [openSnack, setSnack] = useState(false);
   console.log(reviews, "SOY LAS REVIEWS");
-  const classes = useStyles();
   const navigate = useNavigate()
   const myShoes = useSelector((state) => state.detail);
   const userLogged = useSelector((state) => state.user);
@@ -75,7 +49,7 @@ export default function ProductDetail({ id }) {
   // const user = useSelector((state) => state.user);
   const cartProducts = useSelector((state) => state.cart);
 
-  let cantidad = 1;
+
   console.log(myShoes, "soy my shoes");
   const [size, setSize] = useState([]);
   const shoesAdd = {
@@ -280,46 +254,13 @@ export default function ProductDetail({ id }) {
                     resto!
                   </p>
                   <form
-                    className={classes.root}
+                   
                     noValidate
                     autoComplete="off"
                     onSubmit={() => console.log("holaaaaa")}
                   >
-                    <TextField
-                      required
-                      id="outlined-basic"
-                      label="Escribe tu reseña"
-                      variant="outlined"
-                      onChange={handleDescriptionChange}
-                      value={description}
-                      color= "white"
-                    />
+  
                   </form>
-                  <Rating
-                    name="hover-feedback"
-                    value={valueNew}
-                    precision={1}
-                    onChange={(event, newValue) => {
-                      setNewValue(newValue);
-                    }}
-                    onChangeActive={(event, newHover) => {
-                      setHover(newHover);
-                    }}
-                  />
-                  {valueNew !== null && (
-                    <Box ml={0} style={{ marginBottom: "10px" }}>
-                      {labels[hover !== -1 ? hover : valueNew]}
-                    </Box>
-                  )}
-                  <Button
-                    variant="contained"
-                    color="#fd7e14"
-                    style={{ width: "200px" }}
-                    className={styles.addReview}
-                    onClick={handleClickOpen}
-                  >
-                    AÑADIR RESEÑA
-                  </Button>
                 </div>
               )}
 
@@ -330,24 +271,8 @@ export default function ProductDetail({ id }) {
                 reviews.map((e) => (
                   <div className={styles.reviewCard}>
                     <div className={styles.reviewRating}>
-                      <Box
-                        component="fieldset"
-                        mb={1}
-                        borderColor="transparent"
-                      >
-                        <Typography component="legend">Valoracion: </Typography>
-                        <Rating name="read-only" value={e.value} readOnly />
-                        <p style={{ alignSelf: "flex-start" }}>
-                          {" "}
-                          <Chip
-                            variant="outlined"
-                            color="primary"
-                            label={`Autor: ${e.author.name} (Usuario ID: #${e.author.id}) `}
-                            className={styles.chip}
-                            title={e.description}
-                          />{" "}
-                        </p>
-                      </Box>
+                    
+                      
                     </div>
                     <div className={styles.reviewContent}>
                       <span>{e.review}</span>
@@ -358,62 +283,12 @@ export default function ProductDetail({ id }) {
                 <h5>No se encontraron reseñas para este producto.</h5>
               )}
             </div>
-            <Dialog
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="alert-dialog-title"
-              aria-describedby="alert-dialog-description"
-            >
-              <DialogTitle id="alert-dialog-title">
-                {"¿Estas seguro de añadir reseña?"}
-              </DialogTitle>
-              <DialogContent>
-                <DialogContentText
-                  id="alert-dialog-description"
-                  style={{ textAlign: "center", paddingBottom: "5px" }}
-                >
-                  Esta acción puede ser modificada en tu panel de usuario.
-                </DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                <Button
-                  onClick={handleClose}
-                  color="primary"
-                  style={{
-                    maxWidth: "25%",
-                    color: "white",
-                    backgroundColor: "black",
-                  }}
-                >
-                  Cancelar
-                </Button>
-                <Button
-                  onClick={handleCloseDelete}
-                  color="primary"
-                  autoFocus
-                  style={{
-                    maxWidth: "25%",
-                    color: "black",
-                    backgroundColor: "#ffff01",
-                  }}
-                >
-                  Continuar
-                </Button>
-              </DialogActions>
-            </Dialog>
-            <Snackbar
-              open={openSnack}
-              autoHideDuration={6000}
-              onClose={handleSnack}
-            >
-              <Alert
-                onClose={handleSnack}
-                severity="success"
-                style={{ backgroundColor: "#3f51b5", color: "white" }}
-              >
-                La reseña fue añadida con exito
-              </Alert>
-            </Snackbar>
+         
+             
+               
+              
+        
+              
           </Fragment>
         </div>
       ) : (
